9619f1af2de2ff8cb3786ff9ee63c38cbca5c3bc
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2023-12-25 12:29:40 +0530

    automated commit #237

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -29,7 +29,10 @@ resource "aws_launch_template" "template" {
   image_id               = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.security_group.id]
-  user_data = filebase64("${path.module}/userdata.sh")
+  user_data = filebase64(templatefile("${path.module}/userdata.sh",
+    {
+      component = var.component
+    }))
 
   tag_specifications {
     resource_type = "instance"
