33642f03d152f7ef20fe2ae2732c604568b4d990
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2023-12-25 08:34:35 +0530

    automated commit #232

--- main.tf
+++ main.tf
@@ -31,3 +31,13 @@ module "private-id" {
   vpc_id            = module.vpc.vpc_id
 }
 
+module "frontend" {
+  source = "./modules/app"
+  app_port = 80
+  component = frontend
+  env = var.env
+  instance_type = "t2.micro"
+  vpc_cidr = var.vpc_cidr
+  vpc_id = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
+}
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -40,3 +40,18 @@ resource "aws_launch_template" "template" {
     }
   }
 }
+
+
+
+resource "aws_autoscaling_group" "bar" {
+  name               = "${var.env}-${var.component}"
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.template.id
+    version = "$Latest"
+  }
+}
