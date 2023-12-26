97d14b8cb1fda79e8b362c2a357f02bd5753030e
hari chandra prasad chowdary chandra <chandrahari321@gmail.com>
2023-12-26 09:00:53 +0530

    automated commit #242

--- main.tf
+++ main.tf
@@ -21,8 +21,8 @@ module "public-lb" {
   vpc_id            = module.vpc.vpc_id
 }
 
-module "private-id" {
-  source = "./modules/alb"
+module "private-lb" {
+  source            = "./modules/alb"
   alb_sg_allow_cidr = var.vpc_cidr
   alb_type          = "private"
   env               = var.env
@@ -32,25 +32,25 @@ module "private-id" {
 }
 
 module "frontend" {
-  source = "./modules/app"
-  app_port = 80
-  component = "frontend"
-  env = var.env
-  instance_type = "t2.micro"
-  vpc_cidr = var.vpc_cidr
-  vpc_id = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+  source            = "./modules/app"
+  app_port          = 80
+  component         = "frontend"
+  env               = var.env
+  instance_type     = "t2.micro"
+  vpc_cidr          = var.vpc_cidr
+  vpc_id            = module.vpc.vpc_id
+  subnets           = module.vpc.private_subnets
   bastion_node_cidr = var.bastion_node_cidr
 }
 
 module "backend" {
-  source = "./modules/app"
-  app_port = 8080
-  component = "backend"
-  env = var.env
-  instance_type = "t2.micro"
-  vpc_cidr = var.vpc_cidr
-  vpc_id = module.vpc.vpc_id
-  subnets = module.vpc.private_subnets
+  source            = "./modules/app"
+  app_port          = 8080
+  component         = "backend"
+  env               = var.env
+  instance_type     = "t2.micro"
+  vpc_cidr          = var.vpc_cidr
+  vpc_id            = module.vpc.vpc_id
+  subnets           = module.vpc.private_subnets
   bastion_node_cidr = var.bastion_node_cidr
 }
