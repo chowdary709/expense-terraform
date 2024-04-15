4a18604f87881056f3fff8c43e52dc581e72b376
chowdary709 <chandrahari321@gmail.com>
2024-04-15 23:55:35 +0530

    automated commit #281

--- test/main.tf
+++ test/main.tf
@@ -1,9 +1,8 @@
 module "frontend" {
-  source = "./module/app"
-  app_port = 80
-  component = "frontend"
+  source        = "./module/app"
+  app_port      = 80
+  component     = "frontend"
   instance_type = "t2.micro"
-  subnets = "subnet-0233174d26bc6112d"
-  vpc_id = "vpc-0d36408476c035854"
+  subnets       = ["subnet-0233174d26bc6112d"]  # Subnets should be provided as a list
+  vpc_id        = "vpc-0d36408476c035854"
 }
-
