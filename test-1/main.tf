afe5bce44439ef3933f59120b7115a24c686a647
chowdary709 <chandrahari321@gmail.com>
2024-04-18 11:45:25 +0530

    automated commit #320

--- test-1/main.tf
+++ test-1/main.tf
@@ -32,6 +32,53 @@
 #  records = [aws_instance.expence[count.index].private_ip]
 #}
 
+resource "aws_iam_role" "role" {
+  count          = 3
+  name = "${var.instance_name[count.index]}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.instance_name[count.index]}-policy"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "kms:Decrypt",
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "${var.instance_name[count.index]}-role"
+  }
+}
+
+
 resource "aws_instance" "expence" {
   count                         = 3
   ami                           = data.aws_ami.ami.id
