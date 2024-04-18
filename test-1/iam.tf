62a1c997221d4c95773182ba45a004f7dde753d4
chowdary709 <chandrahari321@gmail.com>
2024-04-18 10:57:49 +0530

    automated commit #310

---
+++ test-1/iam.tf
@@ -0,0 +1,44 @@
+resource "aws_iam_role" "role" {
+  name = "workstaation-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "workstaion-policy"
+
+    policy = jsonencode({
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "VisualEditor0",
+          "Effect" : "Allow",
+          "Action" : [
+            "kms:Decrypt",
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "workstation-role"
+  }
+}
--- test-1/main.tf
+++ test-1/main.tf
@@ -1,7 +1,41 @@
+#
+#resource "aws_instance" "expence" {
+#  count                         = 3
+#  ami                           = data.aws_ami.ami.id
+#instance_type = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.micro"
+#  subnet_id                     = var.us-east-1c
+#  vpc_security_group_ids        = var.sg
+#  user_data                     = base64encode(templatefile("${path.module}/userdata.sh", {
+#    role_name = var.instance_name[count.index]  # Use index to get the instance name
+#  }))
+#  tags = {
+#    Name = var.instance_name[count.index]
+#  }
+#
+#instance_market_options {
+#    market_type = "spot"
+#    spot_options {
+#      max_price                        = "0"
+#      instance_interruption_behavior   = "stop"
+#      spot_instance_type               = "persistent"
+#    }
+#  }
+#}
+#
+#
+#resource "aws_route53_record" "record" {
+#  count = 3
+#  zone_id = var.zone_id
+#  name    = "${var.instance_name[count.index]}.${var.domain}" #interpolation
+#  type    = "A"
+#  ttl     = 1
+#  records = [aws_instance.expence[count.index].private_ip]
+#}
+
 resource "aws_instance" "expence" {
   count                         = 3
   ami                           = data.aws_ami.ami.id
-instance_type = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.micro"
+  instance_type                 = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.micro"
   subnet_id                     = var.us-east-1c
   vpc_security_group_ids        = var.sg
   user_data                     = base64encode(templatefile("${path.module}/userdata.sh", {
@@ -11,7 +45,7 @@ instance_type = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.mi
     Name = var.instance_name[count.index]
   }
 
-instance_market_options {
+  instance_market_options {
     market_type = "spot"
     spot_options {
       max_price                        = "0"
@@ -19,8 +53,12 @@ instance_market_options {
       spot_instance_type               = "persistent"
     }
   }
-}
 
+  // Attach IAM role to instances
+  iam_instance_profile {
+    name = aws_iam_role.role.name
+  }
+}
 
 resource "aws_route53_record" "record" {
   count = 3
