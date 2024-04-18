ccede94c194037d5fa5c972adc96410de263207c
chowdary709 <chandrahari321@gmail.com>
2024-04-18 09:24:46 +0530

    automated commit #307

--- test-1/main.tf
+++ test-1/main.tf
@@ -20,7 +20,7 @@ instance_market_options {
 
 
 resource "aws_route53_record" "record" {
-  count = 10
+  count = 3
   zone_id = var.zone_id
   name    = "${var.instance_name[count.index]}.${var.domain}" #interpolation
   type    = "A"
