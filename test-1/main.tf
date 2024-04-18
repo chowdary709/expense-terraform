b7e4359883262e6d3d0555eccd86cba9cd8e0d9a
chowdary709 <chandrahari321@gmail.com>
2024-04-18 11:47:48 +0530

    automated commit #321

--- test-1/main.tf
+++ test-1/main.tf
@@ -34,15 +34,15 @@
 
 resource "aws_iam_role" "role" {
   count          = 3
-  name = "${var.instance_name[count.index]}-role"
+  name           = "${var.instance_name[count.index]}-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Sid       = ""
         Principal = {
           Service = "ec2.amazonaws.com"
         }
@@ -51,15 +51,14 @@ resource "aws_iam_role" "role" {
   })
 
   inline_policy {
-    name = "${var.instance_name[count.index]}-policy"
-
+    name   = "${var.instance_name[count.index]}-policy"
     policy = jsonencode({
-      "Version" : "2012-10-17",
+      "Version"   : "2012-10-17",
       "Statement" : [
         {
-          "Sid" : "VisualEditor0",
-          "Effect" : "Allow",
-          "Action" : [
+          "Sid"     : "VisualEditor0",
+          "Effect"  : "Allow",
+          "Action"  : [
             "kms:Decrypt",
             "ssm:DescribeParameters",
             "ssm:GetParameterHistory",
@@ -67,7 +66,7 @@ resource "aws_iam_role" "role" {
             "ssm:GetParameters",
             "ssm:GetParameter"
           ],
-          "Resource" : "*"
+          "Resource": "*"
         }
       ]
     })
@@ -78,14 +77,13 @@ resource "aws_iam_role" "role" {
   }
 }
 
-
 resource "aws_instance" "expence" {
-  count                         = 3
-  ami                           = data.aws_ami.ami.id
-  instance_type                 = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.micro"
-  subnet_id                     = var.us-east-1c
-  vpc_security_group_ids        = var.sg
-  user_data                     = base64encode(templatefile("${path.module}/userdata.sh", {
+  count                 = 3
+  ami                   = data.aws_ami.ami.id
+  instance_type         = var.instance_name[count.index] == "mysql" ? "t3.medium" : "t2.micro"
+  subnet_id             = var.us-east-1c
+  vpc_security_group_ids= var.sg
+  user_data             = base64encode(templatefile("${path.module}/userdata.sh", {
     role_name = var.instance_name[count.index]  # Use index to get the instance name
   }))
   tags = {
@@ -93,24 +91,28 @@ resource "aws_instance" "expence" {
   }
 
   instance_market_options {
-    market_type = "spot"
+    market_type                    = "spot"
     spot_options {
-      max_price                        = "0"
-      instance_interruption_behavior   = "stop"
-      spot_instance_type               = "persistent"
+      max_price                    = "0"
+      instance_interruption_behavior = "stop"
+      spot_instance_type           = "persistent"
     }
   }
 }
+
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.instance_name[count.index]}-role"
-  role = aws_iam_role.role.name
+  count = 3
+  name  = "${var.instance_name[count.index]}-role"
+  role  = aws_iam_role.role[count.index].name
 }
 
 resource "aws_route53_record" "record" {
-  count = 3
+  count  = 3
   zone_id = var.zone_id
   name    = "${var.instance_name[count.index]}.${var.domain}" #interpolation
   type    = "A"
   ttl     = 1
   records = [aws_instance.expence[count.index].private_ip]
 }
+
+terraform apply -auto-approve
