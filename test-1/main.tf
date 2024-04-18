6a696ddbaa9fe827cb28146d3b2eb2adebdab056
chowdary709 <chandrahari321@gmail.com>
2024-04-18 08:51:49 +0530

    automated commit #297

--- test-1/main.tf
+++ test-1/main.tf
@@ -5,9 +5,7 @@ resource "aws_instance" "expence" {
   subnet_id                     = var.us-east-1c
   associate_public_ip_address   = true
   vpc_security_group_ids        = var.sg
-  user_data                     = base64encode(templatefile("${path.module}/userdata.sh", {
-    role_name = var.instance_name  # Passing instance name instead of type
-  }))
+
   tags = {
     name = var.instance_name
   }
@@ -21,6 +19,10 @@ resource "aws_instance" "expence" {
     }
   }
 }
+
+user_data                     = base64encode(templatefile("${path.module}/userdata.sh", {
+  role_name = var.instance_name  # Passing instance name instead of type
+}))
 
 resource "aws_route53_record" "www" {
   count   = 3
