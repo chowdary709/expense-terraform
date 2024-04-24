ab29a067289d9e00cf7d0ca2966376eee94e9b12
chowdary709 <chandrahari321@gmail.com>
2024-04-24 21:17:06 +0530

    automated commit #350

--- test-vpc/main.tf
+++ test-vpc/main.tf
@@ -47,3 +47,24 @@ resource "aws_subnet" "private_subnets" {
     Name = "private-subnet-${count.index + 1}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "igw"
+  }
+}
+
+resource "aws_route_table" "example" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "igw"
+  }
+}
