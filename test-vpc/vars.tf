87c121686176a0121c35095126d27d7c2714c2bf
chowdary709 <chandrahari321@gmail.com>
2024-04-24 21:10:23 +0530

    automated commit #349

--- test-vpc/vars.tf
+++ test-vpc/vars.tf
@@ -2,12 +2,20 @@ variable "cidr_block" {
   default = "10.0.0.0/16"
 }
 
+#variable "public_subnets" {
+#  default = ["10.0.0.0/24", "10.0.1.0/24"]
+#}
+#
+#variable "private_subnets" {
+#  default = ["10.0.2.0/24", "10.0.3.0/24"]
+#}
+
 variable "public_subnets" {
-  default = ["10.0.0.0/24", "10.0.1.0/24"]
+  default = ["10.0.0.0/24"]
 }
 
 variable "private_subnets" {
-  default = ["10.0.2.0/24", "10.0.3.0/24"]
+  default = ["10.0.1.0/24"]
 }
 
 variable "azs" {
