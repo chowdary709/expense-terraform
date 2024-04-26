ba38770be71e43e393f11002399574de3ecfebb8
chowdary709 <chandrahari321@gmail.com>
2024-04-26 16:29:24 +0530

    automated commit #364

--- test-vpc/main.tf
+++ test-vpc/main.tf
@@ -6,6 +6,27 @@ resource "aws_vpc" "main" {
 }
 
 
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+// Define private subnets
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.azs[count.index]
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+#}
+
 #resource "aws_subnet" "public_subnets" {
 #  count             = length(var.public_subnets)
 #  vpc_id            = aws_vpc.main.id
@@ -26,27 +47,6 @@ resource "aws_vpc" "main" {
 #    Name = "private-subnet-${count.index + 1}"
 #  }
 #}
-
-resource "aws_subnet" "public_subnets" {
-  count             = length(var.public_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.public_subnets[count.index]
-  availability_zone = var.azs[count.index]
-  tags = {
-    Name = "public-subnet-${count.index + 1}"
-  }
-}
-
-// Define private subnets
-resource "aws_subnet" "private_subnets" {
-  count             = length(var.private_subnets)
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnets[count.index]
-  availability_zone = var.azs[count.index]
-  tags = {
-    Name = "private-subnet-${count.index + 1}"
-  }
-}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
