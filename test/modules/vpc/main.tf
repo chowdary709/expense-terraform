9fcf957da8dc74dc6bdf45e10d51aacfeab1e405
chowdary709 <chandrahari321@gmail.com>
2024-05-21 19:18:26 +0530

    update

--- test/modules/vpc/main.tf
+++ test/modules/vpc/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_subnet" {
   availability_zone = var.azs[count.index]
 
   tags = {
-    Name = "${var.env}-public${count.index + 1}"
+    Name = "${var.env}-public$-{count.index + 1}"
   }
 }
 
@@ -23,7 +23,7 @@ resource "aws_subnet" "private_subnet" {
   availability_zone = var.azs[count.index]
 
   tags = {
-    Name = "${var.env}-private${count.index + 1}"
+    Name = "${var.env}-private-${count.index + 1}"
   }
 }
 
