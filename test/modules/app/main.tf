d1b69ed6e0c5f5224218b766a0f2da6ec74f8932
chowdary709 <chandrahari321@gmail.com>
2024-05-23 16:01:15 +0530

    update

--- test/modules/app/main.tf
+++ test/modules/app/main.tf
@@ -4,6 +4,9 @@ resource "aws_launch_template" "template" {
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg.id]
 
+  iam_instance_profile {
+    name = aws_iam_instance_profile.instance_profile.name
+  }
   user_data = base64encode(templatefile("${path.module}/userdata.sh", {
     env       = var.env
     role_name = var.component
