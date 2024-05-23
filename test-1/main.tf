a776b710edfcddfd4dc1b18bd8d8a13e9107c8a7
chowdary709 <chandrahari321@gmail.com>
2024-05-23 17:06:35 +0530

    update

---
+++ test-1/main.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_role" "role" {
+  name = "iam-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "${var.env}-${var.component}-policy"
+
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:DescribeParameters",
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "*"
+        }
+      ]
+    })
+  }
+
+  tags = {
+    tag-key = "iam-dev"
+  }
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "iam-dev"
+  role = aws_iam_role.role.name
+}
