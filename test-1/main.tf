f4c26ada2ddcc5473aae6d9d65af699fce3590b0
chowdary709 <chandrahari321@gmail.com>
2024-05-24 20:31:59 +0530

    update

--- test-1/main.tf
+++ test-1/main.tf
@@ -1,48 +1,72 @@
-resource "aws_iam_role" "role" {
-  name = "iam-dev"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  inline_policy {
-    name = "iam-dev-policy"
-
-    policy = jsonencode({
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Sid": "VisualEditor0",
-          "Effect": "Allow",
-          "Action": [
-            "ssm:DescribeParameters",
-            "ssm:GetParameterHistory",
-            "ssm:GetParametersByPath",
-            "ssm:GetParameters",
-            "ssm:GetParameter"
-          ],
-          "Resource": "*"
-        }
-      ]
-    })
-  }
-
-  tags = {
-    tag-key = "iam-dev"
-  }
+module "vpc" {
+  source                 = "./modules/vpc"
+  vpc_cidr               = var.vpc_cidr
+  env                    = var.env
+  private_subnets        = var.private_subnets
+  public_subnets         = var.public_subnets
+  azs                    = var.azs
+  account_no             = var.account_no
+  default_vpc_id         = var.default_vpc_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_route_table_id = var.default_route_table_id
 }
 
-# resource "aws_iam_instance_profile" "instance_profile" {
-#   name = "iam-dev"
-#   role = aws_iam_role.role.name
-# }
+#module "public-lb" {
+#  source            = "./modules/alb"
+#  alb_sg_allow_cidr = "0.0.0.0/0"
+#  alb_type          = "public"
+#  env               = var.env
+#  internal          = false
+#  subnets           = module.vpc.public_subnets
+#  vpc_id            = module.vpc.vpc_id
+#}
+#
+module "private-lb" {
+  source            = "./modules/alb"
+  alb_sg_allow_cidr = var.vpc_cidr
+  alb_type          = "private"
+  env               = var.env
+  internal          = true
+  subnets           = module.vpc.private_subnets
+  vpc_id            = module.vpc.vpc_id
+  dns_name          = "backend-${var.env}.rdevopsb73.online"
+  zone_id           = "Z09059901XRPHNYMGLMJ4"
+  tg_arn            = module.backend.tg_arn
+}
+
+module "frontend" {
+  source            = "./modules/app"
+  app_port          = 80
+  component         = "frontend"
+  env               = var.env
+  instance_type     = "t3.micro"
+  vpc_cidr          = var.vpc_cidr
+  vpc_id            = module.vpc.vpc_id
+  subnets           = module.vpc.private_subnets
+  bastion_node_cidr = var.bastion_node_cidr
+}
+
+
+module "backend" {
+  depends_on        = [module.mysql]
+  source            = "./modules/app"
+  app_port          = 8080
+  component         = "backend"
+  env               = var.env
+  instance_type     = "t3.micro"
+  vpc_cidr          = var.vpc_cidr
+  vpc_id            = module.vpc.vpc_id
+  subnets           = module.vpc.private_subnets
+  bastion_node_cidr = var.bastion_node_cidr
+}
+
+module "mysql" {
+  source = "./modules/rds"
+
+  component = "mysql"
+  env       = var.env
+  subnets   = module.vpc.private_subnets
+  vpc_cidr  = var.vpc_cidr
+  vpc_id    = module.vpc.vpc_id
+}
+
