b1b82964b7e264e3c7064f71f826e52356b1bcec
chowdary709 <chandrahari321@gmail.com>
2024-05-24 20:33:34 +0530

    update

---
+++ dev/modules/alb/main.tf
@@ -0,0 +1,58 @@
+resource "aws_security_group" "security_group" {
+  name        = "${var.env}-${var.alb_type}-sg"
+  description = "${var.env}-${var.alb_type}-sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.alb_sg_allow_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.alb_type}-sg"
+  }
+}
+
+resource "aws_lb" "alb" {
+  name               = "${var.env}-${var.alb_type}"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = var.subnets
+  tags = {
+    Environment = "${var.env}-${var.alb_type}"
+  }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = var.dns_name
+  type    = "CNAME"
+  ttl     = 300
+  records = [aws_lb.alb.dns_name]
+}
+
+resource "aws_lb_listener" "listener-http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = var.tg_arn
+  }
+}
+
+
+
+
---
+++ dev/vars.tf
@@ -0,0 +1,11 @@
+variable "env" {}
+variable "vpc_cidr" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "azs" {}
+variable "default_vpc_id" {}
+variable "account_no" {}
+variable "default_vpc_cidr" {}
+variable "default_route_table_id" {}
+variable "bastion_node_cidr" {}
+
