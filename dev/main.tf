a7aaaf79334fac82d8db3a926f27d0c91d7e8dcd
chowdary709 <chandrahari321@gmail.com>
2024-05-25 13:42:12 +0530

    update

--- dev/main.tf
+++ dev/main.tf
@@ -11,18 +11,18 @@ module "vpc" {
   default_route_table_id = var.default_route_table_id
 }
 
-# module "public-lb" {
-#   source            = "./modules/alb"
-#   alb_sg_allow_cidr = "0.0.0.0/0"
-#   alb_type          = "public"
-#   env               = var.env
-#   internal          = false
-#   subnets           = module.vpc.public_subnets
-#   vpc_id            = module.vpc.vpc_id
-#   dns_name          = "${var.env}.roboshop.internal"
-#   zone_id           = "Z08360431XA1BOY4SK2N0"
-#   tg_arn            = module.frontend.tg_arn
-# }
+module "public-lb" {
+  source            = "./modules/alb"
+  alb_sg_allow_cidr = "0.0.0.0/0"
+  alb_type          = "public"
+  env               = var.env
+  internal          = false
+  subnets           = module.vpc.public_subnets
+  vpc_id            = module.vpc.vpc_id
+  dns_name          = "${var.env}.roboshop.internal"
+  zone_id           = "Z08360431XA1BOY4SK2N0"
+  tg_arn            = module.frontend.tg_arn
+}
 
 module "private-lb" {
   source            = "./modules/alb"
