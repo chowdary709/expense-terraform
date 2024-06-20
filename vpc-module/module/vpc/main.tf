fea0ef40f27074cafcc347f476f263a39ff26427
chowdary709 <chandrahari321@gmail.com>
2024-06-20 12:41:39 +0530

    automated commit #2767

--- vpc-module/module/vpc/main.tf
+++ vpc-module/module/vpc/main.tf
@@ -1,13 +1,11 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
 
-  tags = {
-    Name = merge(
-      var.common_tags,
-      var.vpc_tags,
-      {
-        Name = "${var.project_name}-${var.env}"
-      }
-    )
-  }
+  tags = merge(
+    var.common_tags,
+    var.vpc_tags,
+    {
+      Name = "${var.project_name}-${var.env}"
+    }
+  )
 }
