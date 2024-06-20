d27feebc1c77ab617bbcc148ce58ac25ec70dcb4
chowdary709 <chandrahari321@gmail.com>
2024-06-20 20:32:33 +0530

    automated commit #2790

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -8,4 +8,6 @@ module "vpc" {
   igw_tags     = var.igw_tags
   public_subnet_cidr = var.public_subnet_cidr
   public_subnet_tags = var.public_subnet_tags
+  private_subnet_cidr = var.private_subnet_cidr
+  private_subnet_tags = var.private_subnet_tags
 }
