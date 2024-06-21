898250f9362722654abab041a9a3aa0e068cefd1
chowdary709 <chandrahari321@gmail.com>
2024-06-21 19:41:19 +0530

    automated commit #2811

--- my_module/main.tf
+++ my_module/main.tf
@@ -1,8 +1,9 @@
 module "vpc" {
-  source         = "./module/vpc"
-  env            = var.env
-  vpc_cidr       = var.vpc_cidr
-  azs            = var.azs
-  private_subnets = var.private_subnets
-  public_subnets = var.public_subnets
+  source           = "./module/vpc"
+  env              = var.env
+  vpc_cidr         = var.vpc_cidr
+  azs              = var.azs
+  private_subnets  = var.private_subnets
+  public_subnets   = var.public_subnets
+  database_subnets = var.database_subnets
 }
