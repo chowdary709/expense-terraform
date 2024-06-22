6e71c92e8d07a418da6c423c8ade720be610fd4e
chowdary709 <chandrahari321@gmail.com>
2024-06-22 14:21:08 +0530

    automated commit #2833

--- dev/modules/app/userdata.sh
+++ dev/modules/app/userdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-dnf install python3.11-pip ansible -y | tee -a /opt/userdata.log
-pip3.11 install boto3 botocore | tee -a /opt/userdata.log
+yum install python3.12-devel python3.12-pip -y | tee -a /opt/userdata.log
+
+pip3.12 install ansible ansible-core==2.16 botocore boto3 python-jenkins | tee -a /opt/userdata.log
+
 ansible-pull -i localhost, -U https://github.com/chowdary709/infra-ansible main.yml -e role_name=${role_name} -e env=${env} | tee -a /opt/userdata.log
